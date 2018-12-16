2936e26dfebeecadf396cefd02364dab4d688f48
zhf <599259501@qq.com>
2018-12-16 22:52:06 +0800

    +初步实现logsegment

--- LinkLog.php
+++ LinkLog.php
@@ -10,14 +10,14 @@ require 'Logger/LinkLogger.php';
 require 'IDCreator/BaseIDCreator.php';
 class LinkLog{
     protected $logger;
+
+    protected $idCreator;
     // 开始记录日志的时间，主要用来统计整个接口所花费的时间
     protected $beginTime;
     // 这里打算采用链表形式存储
     protected $headerSegment;
     // 当前的链路
     protected $currentSegment;
-    // 链路id生成器
-    protected $linkIdCreator;
     // 最低的日志级别
     public $minLevel;
 
@@ -25,9 +25,9 @@ class LinkLog{
 
     private function __construct(LinkLogger $logger,BaseIDCreator $idCreator)
     {
-        $this->logger;
-        $this->beginTime = $this->lastTime = time();
-        $this->linkIdCreator = $idCreator;
+        $this->logger = $logger;
+        $this->idCreator = $idCreator;
+        $this->headerSegment = $this->currentSegment = &(new LogSegment($logger, $idCreator, null));
     }
 
     public static function getLogInstance(LinkLogger $logger,BaseIDCreator $idCreator){
@@ -38,6 +38,11 @@ class LinkLog{
         return self::$LinkLog;
     }
 
+    public function setMiniLevel($level){
+        // todo 这里要对level做判断
+        $this->minLevel = $level;
+    }
+
     public function __call($name, $arguments)
     {
         // TODO: Implement __call() method.
@@ -45,8 +50,10 @@ class LinkLog{
             return $this->$name($arguments);
         }
 
-        if (function_exists($this->logger, $name)) {
-            return $this->logger->$name($arguments);
+        if (function_exists($this->headerSegment, $name)) {
+            $preSegment = $this->currentSegment;
+            $this->currentSegment = &(new LogSegment($this->logger, $this->idCreator, $preSegment));
+            return $this->currentSegment->$name($arguments);
         }
 
         throw new Exception("method not found!");
@@ -57,8 +64,10 @@ class LinkLog{
             return $this->$name($arguments);
         }
 
-        if (function_exists($this->logger, $name)) {
-            return $this->logger->$name($arguments);
+        if (function_exists($this->headerSegment, $name)) {
+            $preSegment = $this->currentSegment;
+            $this->currentSegment = &(new LogSegment($this->logger, $this->idCreator, $preSegment));
+            return $this->currentSegment->$name($arguments);
         }
 
         throw new Exception("method not found!");
--- LogSegment.php
+++ LogSegment.php
@@ -23,15 +23,63 @@ class LogSegment
 
     public $linkIdCreator;
 
+    public $tranceId;
+
+    public $headerSegment;
+
     public function __construct(LinkLogger $logger,BaseIDCreator $idCreator,LogSegment $preSegment)
     {
         $this->logger = $logger;
         $this->linkIdCreator = $idCreator;
         $this->preSegment = $preSegment;
         $this->logTime = time();
+
+        if (is_null($preSegment)) {
+            $this->headerSegment = &$this;
+        }
+
+        $this->tranceId = $idCreator->getLinkId();
     }
 
-    public function debug(){
+    public function debug($file, $message, $context = []){
+        return $this->addRecord(__FUNCTION__, $file, $message, $context);
+    }
 
+    public function info($file, $message, $context = []){
+        return $this->addRecord(__FUNCTION__, $file, $message, $context);
+    }
+
+    public function notice($file, $message, $context = []){
+        return $this->addRecord(__FUNCTION__, $file, $message, $context);
+    }
+
+    public function warn($file, $message, $context = []){
+        return $this->addRecord(__FUNCTION__, $file, $message, $context);
+    }
+
+    public function error($file, $message, $context = []){
+        return $this->addRecord(__FUNCTION__, $file, $message, $context);
+    }
+
+    public function critical($file, $message, $context = []){
+        return $this->addRecord(__FUNCTION__, $file, $message, $context);
+    }
+
+    public function alert($file, $message, $context = []){
+        return $this->addRecord(__FUNCTION__, $file, $message, $context);
+    }
+
+    public function emergency($file, $message, $context = []){
+        return $this->addRecord(__FUNCTION__, $file, $message, $context);
+    }
+
+    public function addRecord($level, $file, $message, $context = []){
+        if (!is_null($this->preSegment)) {
+            $context['cost_time'] = $this->logTime - $this->preSegment->logTime;
+            $context['parent_trance_id'] = $this->tranceId;
+            $context['request_trance_id'] = $this->headerSegment->tranceId;
+        }
+
+        return $this->logger->$level($file, $message, $context);
     }
 }
